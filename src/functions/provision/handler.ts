30dbff0a7ee9c754c0ad7e8cb2bda7ac18993106
Rafael da Rocha <rafael.rocha@omni.chat>
2023-06-11 20:42:54 -0700

    feat: changeStatus await

--- src/functions/provision/handler.ts
+++ src/functions/provision/handler.ts
@@ -2,7 +2,8 @@ import { formatJSONResponse } from '@libs/api-gateway';
 import * as utils from '@libs/utils';
 import { APIGatewayProxyHandler } from 'aws-lambda/trigger/api-gateway-proxy';
 
-//ESSA API é async, basta chamar o processo de aprovisionamento do seu sistema e já retornar 200, não precisa esperar terminar o processo de aprovisionamento
+//ESSA API é async, voce pode chamar outra lambda para fazer o aprovisionamento do seu sistema e já retornar 200 nessa aqui
+// ou voce pode fazer o processo todo aqui, já chamando o changeStatus nessa mesma lambda conforme exemplo abaixo, porém esse processo não pode levar mais que 5 segundos senão vai dar timeout
 export const provision: APIGatewayProxyHandler = async (event) => {
   const order = JSON.parse(event.body);
 
@@ -12,8 +13,8 @@ export const provision: APIGatewayProxyHandler = async (event) => {
 
   // LOGO APÓS TODO PROCESSO DE APROVISIONAMENTO SER EXECUTADO, O STATUS DO PEDIDO DEVE SER ATUALIZADO NA OMNI:
   //TODO: LEMBRE-SE DE GERAR A API SECRET ANTES DE EXECUTAR O SEGUINTE MÉTODO
-  utils.updateOmniChatOrderStatus(order.objectId, "ORDER_GATEWAY_PROVISIONED", 'Pedido Criado!'); 
 
+  await utils.updateOmniChatOrderStatus(order.objectId, "ORDER_GATEWAY_PROVISIONED", 'Pedido Criado!'); 
   return formatJSONResponse({ result: 'ok' });
 };
 
